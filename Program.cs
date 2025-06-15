001d9a976ac9144c34ab790c3763889daaebba40
VladGars <kn1b23.harasevych@kpnu.edu.ua>
2025-06-15 12:50:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// 1. Створення інтерфейсу
+public interface IDisplayable
+{
+    void Display();
+}
+
+// 2. Реалізація в базовому класі
+public class Shape : IDisplayable
+{
+    public virtual void Display()
+    {
+        Console.WriteLine("Це загальна фігура.");
+    }
+}
+
+// 3. Перевизначення в похідному класі
+public class Circle : Shape
+{
+    public override void Display()
+    {
+        Console.WriteLine("Це коло.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape myShape = new Shape();
+        Circle myCircle = new Circle();
+
+        myShape.Display();  // Виклик реалізації базового класу
+        myCircle.Display(); // Виклик перевизначеної реалізації
+    }
+}
